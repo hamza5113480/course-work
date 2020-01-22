1af9e1c679f4109e0bef5619a90787db4d043561
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-01-23 02:48:40 +0500

    Program that prompts the user to enter two numbers and performs addition, subtraction, multiplication, and division operations on them. Print the results of these operations.

---
+++ fundamentals-of-programming/tasks/SimpleCalculator.java
@@ -0,0 +1,26 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        if (num2 != 0) {
+            System.out.println("Division: " + (num1 / num2));
+        } else {
+            System.out.println("Division by zero is not allowed.");
+        }
+
+        scanner.close();
+    }
+}
