2a33d84c29197c2fb6bf7bf879b8db75262c732f
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-03-19 02:52:23 +0500

    Program that defines a function to calculate the factorial of a non-negative integer. Prompt the user to enter a number and then call the function to calculate and print its factorial.

---
+++ fundamentals-of-programming/tasks/FactorialFunction.java
@@ -0,0 +1,23 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class FactorialFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Factorial of " + number + " is: " + factorial(number));
+
+        scanner.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
