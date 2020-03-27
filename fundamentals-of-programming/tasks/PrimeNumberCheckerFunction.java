2be3811f975acb85bcf7a3ac8afcc9643d36e7c4
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-03-28 02:52:10 +0500

    Program that defines a function to check if a given number is prime. Prompt the user to enter a number and then call the function to check if it's prime.

---
+++ fundamentals-of-programming/tasks/PrimeNumberCheckerFunction.java
@@ -0,0 +1,32 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class PrimeNumberCheckerFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
