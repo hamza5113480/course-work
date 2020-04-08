ae0746b7d6565762d91bcf1280b807bbce87637e
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-04-09 02:54:02 +0500

    Program that defines a function to calculate the greatest common divisor of two numbers using the Euclidean algorithm. Prompt the user to enter two numbers and then call the function to calculate and print the GCD.

---
+++ fundamentals-of-programming/tasks/GCDFunction.java
@@ -0,0 +1,28 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class GCDFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd(num1, num2));
+
+        scanner.close();
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
