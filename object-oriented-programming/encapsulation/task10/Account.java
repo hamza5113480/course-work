11acb2c9ec514e62296c6d19ea0aa8d03ba8ec80
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-07-17 20:57:25 +0500

    Account Class

---
+++ object-oriented-programming/encapsulation/task10/Account.java
@@ -0,0 +1,43 @@
+package task10;
+
+class Account {
+    private String accountNumber;
+    private double balance;
+    private String accountHolderName;
+
+    public Account(String accountNumber, double balance, String accountHolderName) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.accountHolderName = accountHolderName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit of " + amount + " successful. New balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal of " + amount + " successful. New balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds or invalid withdrawal amount.");
+        }
+    }
+}
