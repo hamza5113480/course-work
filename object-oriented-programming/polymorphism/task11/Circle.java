96a5f3d3f8c924bf98f95a13625f5db92507f6e5
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-07-25 21:03:37 +0500

    Circle: Subclass of Shape representing a circle with additional attributes like radius and methods to calculate area and perimeter.

---
+++ object-oriented-programming/polymorphism/task11/Circle.java
@@ -0,0 +1,27 @@
+package task11;
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    void printDetails() {
+        System.out.println("Circle Details:");
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+    }
+}
