2b0e3078e80cc5a230217ff4256721c545714b89
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-07-25 21:03:43 +0500

    Rectangle: Subclass of Shape representing a rectangle with additional attributes like length, width, and methods to calculate area and perimeter.

---
+++ object-oriented-programming/polymorphism/task11/Rectangle.java
@@ -0,0 +1,30 @@
+package task11;
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double getArea() {
+        return length * width;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * (length + width);
+    }
+
+    @Override
+    void printDetails() {
+        System.out.println("Rectangle Details:");
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
+    }
+}
