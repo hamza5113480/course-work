eb0c6f660db24c57285211bf75f3fae968eaa867
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-08-23 21:05:21 +0500

    Account

---
+++ object-oriented-programming/polymorphism/task15/Account.java
@@ -0,0 +1,15 @@
+package task15;
+
+abstract class Account {
+    protected String accountNumber;
+    protected double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    abstract double calculateInterest();
+
+    abstract void displayDetails();
+}
