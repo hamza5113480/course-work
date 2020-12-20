80358ace45ca583d02347f0ed2c762a731e6c150
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2020-12-20 21:51:35 +0500

    Infix to postfix

---
+++ datastructures-and-algortihm/applications-of-stacks/task-1/task-5.1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+// Function to check if a character is an operator
+bool isOperator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
+}
+
+// Function to get precedence of an operator
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+
+// Function to convert infix expression to postfix notation
+std::string infixToPostfix(const std::string& infix) {
+    std::stack<char> operators;
+    std::string postfix;
+
+    for (char ch : infix) {
+        if (isalpha(ch) || isdigit(ch)) {
+            postfix += ch;
+        } else if (ch == '(') {
+            operators.push(ch);
+        } else if (ch == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.pop(); // Discard '('
+        } else if (isOperator(ch)) {
+            while (!operators.empty() && precedence(operators.top()) >= precedence(ch)) {
+                postfix += operators.top();
+                operators.pop();
+            }
+            operators.push(ch);
+        }
+    }
+
+    while (!operators.empty()) {
+        postfix += operators.top();
+        operators.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    std::string infixExpression;
+    std::cout << "Enter an infix expression: ";
+    std::getline(std::cin, infixExpression);
+
+    std::string postfixExpression = infixToPostfix(infixExpression);
+    std::cout << "Postfix expression: " << postfixExpression << std::endl;
+
+    return 0;
+}
