14fc8c89db5a6a95c15b7c088bd7536117303b8f
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-03-24 00:57:18 +0500

    Write SQL statements to insert seven more records into same table and also attach printed output of table.

--- databases/README.md
+++ databases/README.md
@@ -3,3 +3,4 @@
 1. Write SQL statements to create a table name “STUDENT” of following table and insert data shown below.
    
       ![img.png](../resources/img.png)
+2. Write SQL statements to insert seven more records into same table and also attach printed output of table.
--- databases/lab-1.sql
+++ databases/lab-1.sql
@@ -16,3 +16,12 @@ Insert into STUDENT values(1237, 'Aysha', 'Abbas','D-26','Karachi','Pakistan','1
 Insert into STUDENT values(1238, 'Saleem', 'Ahmed','D-27','Rawalpindi','Pakistan','10-Dec-1978','M');
 Insert into STUDENT values(1239, 'Fawad', 'Alam','D-28','Lahore','Pakistan','10-Nov-1986','M');
 Insert into STUDENT values(1240, 'Hina', 'khan','D-28','Karachi','Pakistan','10-Oct-1988','F');
+
+--> Write SQL statements to insert seven more records into same table and also attach printed output of table.
+Insert into STUDENT values(1240, 'hamza', 'Kashif','D-23','Lahore','Pakistan','10-Sep-1958','M');
+Insert into STUDENT values(1241, 'abid', 'Sandal','D-30','Lahore','Pakistan','10-Sep-1956','M');
+Insert into STUDENT values(1242, 'Aysha', 'Abbas','D-31','Islamabad','Pakistan','10-Jan-2000','F');
+Insert into STUDENT values(1243, 'Naseer', 'Ahmed','D-32','bahawalpur','Pakistan','10-Dec-1998','M');
+Insert into STUDENT values(1244, 'Saleem', 'Ahmed','D-33','rahimyarkhan','Pakistan','10-Nov-1996','M');
+Insert into STUDENT values(1245, 'hassan', 'khan','D-34','kotli','Pakistan','10-Oct-1998','F');
+Insert into STUDENT values(1246, 'bilal', 'khan','D-35','kashmir','Pakistan','10-Oct-1978','F');
