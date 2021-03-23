95da44f888c0af192dd1bed4e2f61a466c4f04fd
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-03-24 00:52:21 +0500

    Write SQL statements to create a table name “STUDENT” of following table and insert data

---
+++ databases/lab-1.sql
@@ -0,0 +1,18 @@
+-->  Write SQL statements to create a table name “STUDENT” of following table and insert data
+ CREATE TABLE STUDENT
+(
+    Reg_NO integer,
+    First_Name char(50),
+    Last_Name char(50),
+    Address char(50),
+    City char(50),
+    Country char(25),
+    Birth_Date date,
+    Gender char(50),
+);
+Insert into STUDENT values(1234, 'Nasir', 'Kareem','D-23','Lahore','Pakistan','10-Sep-1988','M');
+Insert into STUDENT values(1235, 'Shahid', 'Sultan','D-58','Lahore','Pakistan','10-Sep-1989','M');
+Insert into STUDENT values(1237, 'Aysha', 'Abbas','D-26','Karachi','Pakistan','10-Jan-1990','F');
+Insert into STUDENT values(1238, 'Saleem', 'Ahmed','D-27','Rawalpindi','Pakistan','10-Dec-1978','M');
+Insert into STUDENT values(1239, 'Fawad', 'Alam','D-28','Lahore','Pakistan','10-Nov-1986','M');
+Insert into STUDENT values(1240, 'Hina', 'khan','D-28','Karachi','Pakistan','10-Oct-1988','F');
