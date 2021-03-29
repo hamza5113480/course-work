445d3f330a21a1fa11ffbeaa445164e765acb747
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-03-30 00:57:41 +0500

    Create a table called EMPLOYEE table and allow NULL for all columns except EMPNO, ENAME, DOJ and JOB.

---
+++ databases/lab-2.sql
@@ -0,0 +1,13 @@
+--> Create a table called EMPLOYEE table and allow NULL for all columns except EMPNO, ENAME, DOJ and JOB.
+CREATE TABLE EMPLOYEE (
+    EMPNO NUMERIC(6) NOT NULL,
+    ENAME VARCHAR(10) NOT NULL,
+    GENDER CHAR(1) NOT NULL,
+    DOJ DATE NOT NULL,
+    JOB VARCHAR(30) NOT NULL,
+    MGR NUMERIC(4),
+    DEPTNO NUMERIC(3),
+    SAL NUMERIC(7, 2),
+    PRIMARY KEY (EMPNO)
+);
+
