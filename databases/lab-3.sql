f807f1e2e8d1702229b25c94513bd1d1b363844b
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-04-10 12:12:32 +0500

    Write a query to add constraints to the EMPLOYEE table that EMPNO as the primary key and DEPTNO as the foreign key.

--- databases/lab-3.sql
+++ databases/lab-3.sql
@@ -7,3 +7,7 @@ CREATE TABLE DEPARTMENT (
     CONSTRAINT pk_deptno PRIMARY KEY (DEPTNO)
 );
 
+--> Write a query to add constraints to the EMPLOYEE table that EMPNO as the primary key and DEPTNO as the foreign key.
+ALTER TABLE EMPLOYEE ADD CONSTRAINT pk_empno PRIMARY KEY (EMPNO);
+ALTER TABLE EMPLOYEE ADD CONSTRAINT fk_deptno FOREIGN KEY (DEPTNO) REFERENCES DEPARTMENT(DEPTNO);
+
