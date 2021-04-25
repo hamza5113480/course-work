f80c496b388b80eee5500b8b8243ad00330d3a20
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-04-25 11:16:02 +0500

    Display all the details of department, whose name does not start with 'E' using LIKE operator.

--- databases/lab-5.sql
+++ databases/lab-5.sql
@@ -1,4 +1,5 @@
 --> Select all employees, whose department name is "EE" and "CSE" using IN operator.
 SELECT * FROM EMPLOYEE WHERE DEPTNO IN (SELECT DEPTNO FROM DEPARTMENT WHERE DNAME IN ('EE', 'CSE'));
 
-
+--> Display all the details of department, whose name does not start with 'E' using LIKE operator.
+SELECT * FROM DEPARTMENT WHERE DNAME NOT LIKE 'E%';
