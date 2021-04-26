eef7ac7f6177874de6e20a9af6a105e73d42173e
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-04-26 22:01:26 +0500

    Calculate the 4th times salary of all employees, who are woking as ‘Manager’ and diplay complete data of employee ;

--- databases/lab-5.sql
+++ databases/lab-5.sql
@@ -3,3 +3,7 @@ SELECT * FROM EMPLOYEE WHERE DEPTNO IN (SELECT DEPTNO FROM DEPARTMENT WHERE DNAM
 
 --> Display all the details of department, whose name does not start with 'E' using LIKE operator.
 SELECT * FROM DEPARTMENT WHERE DNAME NOT LIKE 'E%';
+
+--> Calculate the 4th times salary of all employees, who are woking as ‘Manager’ and diplay complete data of employee ;
+SELECT *, SAL * 4 AS FourthTimesSalary FROM EMPLOYEE WHERE JOB = 'Manager';
+
