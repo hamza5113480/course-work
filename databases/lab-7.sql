c1ed35a8645cc7d60f96d63f7c29861749dd25ba
Syed Hamza Ali Shah <hamza5113480@gmail.com>
2021-05-13 14:15:16 +0500

    Display the departments that are accredited by the quality council.

--- databases/lab-7.sql
+++ databases/lab-7.sql
@@ -9,3 +9,8 @@ CREATE TABLE AccDept (
 INSERT INTO AccDept (DEPTNO, DNAME, DCity) VALUES (10, 'EE', 'MAIN BLOCK');
 INSERT INTO AccDept (DEPTNO, DNAME, DCity) VALUES (20, 'CSE', 'MANUFACTURING UNIT');
 INSERT INTO AccDept (DEPTNO, DNAME, DCity) VALUES (30, 'IS', 'MAIN BLOCK');
+
+-- Display the departments that are accredited by the quality council.
+SELECT dept.DEPTNO,dept.DNAME,dept.DCity FROM AccDept as dept INNER JOIN AccDept
+ON dept.DEPTNO = AccDept.DEPTNO
+WHERE AccDept.DNAME = 'QC'
